8fd2a4cca5b0559061bb57cf1cd324e8c22cab2c
CireSnave <ciresnave@yahoo.com>
2025-09-13 06:50:15 -0700

    chore(deps): remove accidentally re-added vendor/num-bigint-dig

--- vendor/num-bigint-dig/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    // Inform rustc's check-cfg that `has_i128` is an expected cfg used in the
-    // vendored crate. This prevents the "unexpected `cfg` condition name: `has_i128`"
-    // warnings emitted by recent rustc versions when checking cfg attributes.
-    println!("cargo:rustc-check-cfg=cfg(has_i128)");
-}
